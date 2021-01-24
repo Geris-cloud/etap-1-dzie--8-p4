a591670c547e742060723c2fe6604c90719e007f
Dariusz Ochotny <dariuszochotny9@gmail.com>
2021-01-24 23:41:45 +0100

    Update hamburger

--- hamburger.js
+++ hamburger.js
@@ -1,10 +1,23 @@
-const hamburgerIcon = document.querySelector('i');
-const hamburgerMenu = document.querySelector('.menu');
-
+const hamburgerIcon = document.querySelector('nav.hamburger i');
+const hamburgerMenu = document.querySelector('nav div.menu');
+const hamburgerObjectOne = document.querySelector('.one');
+const hamburgerObjectTwo = document.querySelector('.two');
+const hamburgerObjectThree = document.querySelector('.three');
+const hamburgerObjectFour = document.querySelector('.four');
+const hamburgerObjectReservation = document.querySelector('.reservationtwo');
 
 function pullMenu() {
     hamburgerMenu.classList.toggle('pull');
-
+    hamburgerObjectOne.classList.toggle('pull');
+    hamburgerObjectTwo.classList.toggle('pull');
+    hamburgerObjectThree.classList.toggle('pull');
+    hamburgerObjectFour.classList.toggle('pull');
+    hamburgerObjectReservation.classList.toggle('pull');
 }
 
 hamburgerIcon.addEventListener('click', pullMenu);
+hamburgerObjectOne.addEventListener('click', pullMenu);
+hamburgerObjectTwo.addEventListener('click', pullMenu);
+hamburgerObjectThree.addEventListener('click', pullMenu);
+hamburgerObjectFour.addEventListener('click', pullMenu);
+hamburgerObjectReservation.addEventListener('click', pullMenu);
