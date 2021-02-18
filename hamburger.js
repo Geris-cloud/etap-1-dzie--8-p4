1dca518a0755c01d31ea2bdc3cb95e8916b08277
Dariusz Ochotny <dariuszochotny9@gmail.com>
2021-02-18 14:30:08 +0100

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,4 +1,4 @@
 const hamburgerObjects = [...document.querySelectorAll('nav.hamburger i, div.menu a')];
 const hamburgerMenu = document.querySelector('nav div.menu');
 
-hamburgerObjects.forEach((item) => item.addEventListener('click', () => hamburgerMenu.classList.toggle('pull')));
+hamburgerObjects.forEach((object) => object.addEventListener('click', () => hamburgerMenu.classList.toggle('pull')));
